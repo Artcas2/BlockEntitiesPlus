f34679f1cf1b59a597187a17726f7a0a58d23bb9
Artcas2 <castagnejuliette6@gmail.com>
2024-07-16 10:19:21 +0200

    Update plugin info

--- src/main/java/fr/artcas2/BlockEntitiesPlus/Constants/Commands.java
+++ src/main/java/fr/artcas2/BlockEntitiesPlus/Constants/Commands.java
@@ -2,7 +2,7 @@ package fr.artcas2.BlockEntitiesPlus.Constants;
 
 public class Commands {
 
-    public static final String BASE = "blockentities";
+    public static final String BASE = "blockentitiesplus";
     public static final String HELP = "help";
     public static final String RELOAD = "reload";
     public static final String GET = "get";
--- src/main/java/fr/artcas2/BlockEntitiesPlus/Constants/Permissions.java
+++ src/main/java/fr/artcas2/BlockEntitiesPlus/Constants/Permissions.java
@@ -1,8 +1,8 @@
 package fr.artcas2.BlockEntitiesPlus.Constants;
 
 public class Permissions {
-    public static String RELOAD = "blockentities.reload";
-    public static String GET = "blockentities.get";
-    public static String PLACE = "blockentities.place";
-    public static String BREAK = "blockentities.break";
+    public static String RELOAD = "blockentitiesplus.reload";
+    public static String GET = "blockentitiesplus.get";
+    public static String PLACE = "blockentitiesplus.place";
+    public static String BREAK = "blockentitiesplus.break";
 }
